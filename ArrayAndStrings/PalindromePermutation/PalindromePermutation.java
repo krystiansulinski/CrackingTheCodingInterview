670e07495d4e7fbc770d53fe5d98ba80ebb0b248
ksulinski <krystian.sulinski@gmail.com>
2019-11-20 06:23:26 +1000

    Add 1.4 (Second Solution)

--- ArrayAndStrings/PalindromePermutation/PalindromePermutation.java
+++ ArrayAndStrings/PalindromePermutation/PalindromePermutation.java
@@ -1,26 +1,42 @@
 package PalindromePermutation;
 
 public class PalindromePermutation {
-	// Time: O(str.length);
+	// Time: O(str.length)
 	// Space: O(1)
-	public static boolean palindromePermutation(String str) {
-		int[] count = new int[26]; // assume it's English alphabet
+	public static boolean palindromePermutationBitVector(String str) {
+		int bitVector = 0; // assume an English alphabet
 
 		for (char c : str.toLowerCase().toCharArray()) {
 			if ('a' <= c && c <= 'z') { // ignore anything outside of small letters
-				count['z' - c]++;
+				int mask = 1 << c - 'a';
+				if ((bitVector & mask) == 0) {
+					bitVector |= mask; // merge
+				} else {
+					bitVector &= ~mask; // flip
+				}
 			}
 		}
 
-		boolean oddCount = false;
-		for (int c : count) {
-			if (c % 2 != 0) {
-				if (oddCount) {
-					return false;
+		return (bitVector & (bitVector - 1)) == 0; // check exactly one bit set
+	}
+
+	// Time: O(str.length)
+	// Space: O(1)
+	public static boolean palindromePermutation(String str) {
+		int[] count = new int[26]; // assume an English alphabet
+
+		int oddCount = 0;
+		for (char c : str.toLowerCase().toCharArray()) {
+			if ('a' <= c && c <= 'z') { // ignore anything outside of small letters
+				count['z' - c]++;
+				if (count['z' - c] % 2 == 1) {
+					oddCount++;
+				} else {
+					oddCount--;
 				}
-				oddCount = true;
 			}
 		}
-		return true;
+
+		return oddCount < 2;
 	}
 }
--- ArrayAndStrings/PalindromePermutation/PalindromePermutationTest.java
+++ ArrayAndStrings/PalindromePermutation/PalindromePermutationTest.java
@@ -6,11 +6,25 @@ import static org.junit.Assert.assertEquals;
 public class PalindromePermutationTest {
 	@Test
 	public void palindromePermutationTest() {
+		assertEquals(true, PalindromePermutation.palindromePermutation("aa"));
+		assertEquals(true, PalindromePermutation.palindromePermutationBitVector("aa"));
+		
 		assertEquals(true, PalindromePermutation.palindromePermutation("Tact Coa"));
+		assertEquals(true, PalindromePermutation.palindromePermutationBitVector("Tact Coa"));
+		
 		assertEquals(false, PalindromePermutation.palindromePermutation("Tactz Coa"));
+		assertEquals(false, PalindromePermutation.palindromePermutationBitVector("Tactz Coa"));
+		
 		assertEquals(true, PalindromePermutation.palindromePermutation("No lemon, no melon"));
+		assertEquals(true, PalindromePermutation.palindromePermutationBitVector("No lemon, no melon"));
+		
 		assertEquals(true, PalindromePermutation.palindromePermutation("Eva, can I see bees in a cave?"));
+		assertEquals(true, PalindromePermutation.palindromePermutationBitVector("Eva, can I see bees in a cave?"));
+		
 		assertEquals(false, PalindromePermutation.palindromePermutation("Ava, can I see bees in a cave?"));
+		assertEquals(false, PalindromePermutation.palindromePermutationBitVector("Ava, can I see bees in a cave?"));
+		
 		assertEquals(true, PalindromePermutation.palindromePermutation("   "));
+		assertEquals(true, PalindromePermutation.palindromePermutationBitVector("   "));
 	}
 }
