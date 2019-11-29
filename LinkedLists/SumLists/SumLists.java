a7dc498d150c2f879455ced11e7b6788c470a809
ksulinski <krystian.sulinski@gmail.com>
2019-11-29 17:31:00 +1000

    2.5 WIP

--- LinkedLists/SumLists/SumLists.java
+++ LinkedLists/SumLists/SumLists.java
@@ -2,8 +2,8 @@
  * 2.5 Sum Lists
  * 
  * You have two numbers represented by a linked list, where each node contains a single
- * digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a
- * function that adds the two numbers and returns the sum as a linked list.
+ * digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list.
+ * Write a function that adds the two numbers and returns the sum as a linked list.
  * 
  * EXAMPLE
  * 
@@ -21,38 +21,110 @@
 
 package SumLists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import LinkedListNode.LinkedListNode;
 
 public class SumLists {
 	public static LinkedListNode sumLists(LinkedListNode a, LinkedListNode b) {
-		LinkedListNode sum = a;
-		LinkedListNode p = sum;
+		LinkedListNode dummyHead = new LinkedListNode(0);
+		LinkedListNode current = dummyHead;
 		int carry = 0;
 
-		while (a != null && b != null) {
-			int s = getValue(a) + getValue(b);
-			sum.value = carry + (s % 10);
-			carry = s / 10;
-			
-			a = a.next;
-			b = b.next;
-			if (sum.next != null) {
-				sum = sum.next;
-			} 
-		}
-		
-		if (carry == 1) {
-			if (sum.value == 9) {
-				sum.next = new LinkedListNode(1);
-			} else {
-				sum.value++;
+		while (a != null || b != null) {
+			int sum = carry + getValue(a) + getValue(b);
+			current.next = new LinkedListNode(sum % 10);
+			carry = sum / 10;
+
+			if (a != null) {
+				a = a.next;
 			}
+			if (b != null) {
+				b = b.next;
+			}
+			current = current.next;
 		}
 
-		return p;
+		if (carry == 1) {
+			current.next = new LinkedListNode(1);
+		}
+
+		return dummyHead.next;
 	}
 
 	private static int getValue(LinkedListNode node) {
 		return node != null ? node.value : 0;
+	}
+
+	public static LinkedListNode sumListsFollowUp(LinkedListNode a, LinkedListNode b) {
+		LinkedListNode dummyHead = new LinkedListNode(0);
+		LinkedListNode current = dummyHead;
+		List<Integer> carries = new ArrayList<>();
+		
+		int diff = getSize(a) - getSize(b);
+		if (diff > 0) {
+			LinkedListNode dummyB = new LinkedListNode(0);
+			for (int i = 0; i < diff; i++) {
+				dummyB = dummyB.next;
+				dummyB = new LinkedListNode(0);
+			}
+			dummyB.next = b;
+			sum(a, dummyB, current, carries);
+		} else if (diff < 0){
+			LinkedListNode dummyA = new LinkedListNode(0);
+			for (int i = 0; i > diff; i--) {
+				dummyA = dummyA.next;
+				dummyA = new LinkedListNode(0);
+			}
+			dummyA.next = a;
+			sum(dummyA, b, current, carries);
+		} else {
+			sum(a, b, current, carries);
+		}
+
+		// 8 -> 8 -> 8
+		// [1, 1, 1]
+		current = dummyHead;
+		if (carries.get(0) == 1) {
+			LinkedListNode next = current.next;
+			current.next = new LinkedListNode(1);
+			current = current.next;
+			current.next = next;
+		}
+
+		for (int i = 1; i < carries.size(); i++) {
+			if (carries.get(i) == 1) {
+				current.next.value++;
+				current = current.next;
+			}
+		}
+
+		return dummyHead.next;
+	}
+
+	private static void sum(LinkedListNode a, LinkedListNode b, LinkedListNode current, List<Integer> carries) {
+		while (a != null || b != null) {
+			int sum = getValue(a) + getValue(b);
+			current.next = new LinkedListNode(sum % 10);
+			carries.add(sum / 10);
+
+			if (a != null) {
+				a = a.next;
+			}
+			if (b != null) {
+				b = b.next;
+			}
+			current = current.next;
+		}
+	}
+
+	private static int getSize(LinkedListNode node) {
+		int size = 0;
+		while (node != null) {
+			size++;
+			node = node.next;
+		}
+		return size;
 	}
 }
--- LinkedLists/SumLists/SumListsTest.java
+++ LinkedLists/SumLists/SumListsTest.java
@@ -7,7 +7,7 @@ import LinkedListNode.LinkedListNode;
 
 public class SumListsTest {
 	@Test
-	public void partitionTest() {
+	public void sumListsTest() {
 		int[][] values1 = { { 7, 1, 6}, {5, 9, 2}, {2, 1, 9}};
 		int[][] values2 = { { 9, 9, 9}, {9, 9, 9}, {8, 9, 9, 1}};
 		int[][] values3 = { { 7, 1, 6}, {9, 9}, {6, 1, 7}};
@@ -18,11 +18,31 @@ public class SumListsTest {
 		getSum(values3);
 		getSum(values4);
 	}
-
+	
 	private void getSum(int[][] values) {
 		LinkedListNode a = new LinkedListNode(values[0]);
 		LinkedListNode b = new LinkedListNode(values[1]);
 		
 		assertEquals(new LinkedListNode(values[2]), SumLists.sumLists(a, b));
+	}
+	
+	@Test
+	public void sumListsFollowUpTest() {
+		int[][] values1 = { { 6, 1, 7}, {2, 9, 5}, {9, 1, 2}};
+		int[][] values2 = { { 9, 9, 9}, {9, 9, 9}, {1, 9, 9, 8}};
+		int[][] values3 = { {9, 9}, { 7, 1, 6}, {8, 1, 5}};
+		int[][] values4 = { { 7, 1, 6, 5}, {9}, {6, 2, 6, 5}};
+		
+//		getSumFollowUp(values1);
+//		getSumFollowUp(values2);
+//		getSumFollowUp(values3);
+		getSumFollowUp(values4);
+	}
+
+	private void getSumFollowUp(int[][] values) {
+		LinkedListNode a = new LinkedListNode(values[0]);
+		LinkedListNode b = new LinkedListNode(values[1]);
+		
+		assertEquals(new LinkedListNode(values[2]), SumLists.sumListsFollowUp(a, b));
 	}
 }
