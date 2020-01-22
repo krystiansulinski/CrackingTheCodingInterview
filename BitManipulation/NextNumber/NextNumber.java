8056842233e19ce08181ba8f1dc89f00fb2287df
ksulinski <krystian.sulinski@gmail.com>
2020-01-22 13:17:50 +0100

    Add 5.4a (7.5h)

--- BitManipulation/NextNumber/NextNumber.java
+++ BitManipulation/NextNumber/NextNumber.java
@@ -8,42 +8,41 @@
 package NextNumber;
 
 public class NextNumber {
-	public static int largest(int num) {
-		return fillLeftBits(bitCount(num));
-	}
-
-	public static int smallest(int num) {
-		return fillRightBits(bitCount(num));
-	}
-
-	private static int fillLeftBits(int bits) {
-		int largest = 0;
-		int one = 1 << Integer.SIZE - 2; // 2^30
-
-		for (int i = 0; i < bits; i++) {
-			largest += one;
-			one = one >> 1;
+	public static int getNextSmallest(int value) {
+		if (value < 1) {
+			return -1;
 		}
-		return largest;
+
+		int index = getRightmostZeroOneBitSequenceIndex(value);
+		if (index == -1) {
+			return -1;
+		}
+		return swapBitsInRightmostZeroOne(value, index);
 	}
 
-	private static int fillRightBits(int bits) {
-		int smallest = 0;
-		int one = 1;
-
-		for (int i = 0; i < bits; i++) {
-			smallest += one;
-			one = one << 1;
+	private static int getRightmostZeroOneBitSequenceIndex(int value) {
+		for (int pos = 0; pos < Integer.BYTES * Byte.SIZE - 2; pos++) {
+			if (getBit(value, pos) && getBit(value, pos + 1) == false) {
+				return pos;
+			}
 		}
-		return smallest;
+		return -1;
 	}
 
-	private static int bitCount(int num) {
-		int count = 0;
-		while (num != 0) {
-			count = count + (num & 1); // same as count = count + num % 2
-			num = num >>> 1;
-		}
-		return count;
+	private static int swapBitsInRightmostZeroOne(int value, int index) {
+		int num = clearBit(value, index);
+		return setBit(num, index + 1);
+	}
+
+	private static boolean getBit(int value, int index) {
+		return (value & (1 << index)) != 0;
+	}
+
+	private static int setBit(int value, int index) {
+		return value | (1 << index);
+	}
+
+	private static int clearBit(int value, int index) {
+		return value & ~(1 << index);
 	}
 }
--- BitManipulation/NextNumber/NextNumberTest.java
+++ BitManipulation/NextNumber/NextNumberTest.java
@@ -2,28 +2,35 @@ package NextNumber;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 public class NextNumberTest {
 	@Test
-	public void largestTest() {
-		int[] nums = { 0, 1, 3, 8, Integer.MAX_VALUE };
+	public void getNextSmallestTest() {
+		Map<Integer, Integer> pairs = new HashMap<>();
+		pairs.put(0b0, -1);
+		pairs.put(0b1, 0b10);
+		pairs.put(0b10, 0b100);
+		pairs.put(0b11, 0b101);
+		pairs.put(0b100, 0b1000);
+		pairs.put(0b101, 0b110);
+		pairs.put(0b110, 0b1010);
+		pairs.put(0b111, 0b1011);
+		pairs.put(0b1000, 0b10000);
+		pairs.put(0b1001, 0b1010);
+		pairs.put(0b1010, 0b1100);
+		pairs.put(0b1011, 0b1101);
+		pairs.put(0b1100, 0b10100);
+		pairs.put(0b1101, 0b1110);
+		pairs.put(0b1110, 0b10110);
+		pairs.put(0b1111, 0b10111);
+		pairs.put((int) Math.pow(2, 29), (int) Math.pow(2, 30));
+		pairs.put((int) Math.pow(2, 30), -1);
+		pairs.put(0b101010110111111011111111111111, 0b101010110111111101111111111111);
 
-		assertEquals(0, NextNumber.largest(nums[0]));
-		assertEquals((int) Math.pow(2, 30), NextNumber.largest(nums[1]));
-		assertEquals(((int) Math.pow(2, 30)) + ((int) Math.pow(2, 29)), NextNumber.largest(nums[2]));
-		assertEquals((int) Math.pow(2, 30), NextNumber.largest(nums[3]));
-	}
-	
-	@Test
-	public void smallestTest() {
-		int[] nums = { 0, 1, 3, 8, Integer.MAX_VALUE, 11 };
-
-		assertEquals(nums[0], NextNumber.largest(nums[0]));
-		assertEquals(nums[1], NextNumber.smallest(nums[1]));
-		assertEquals(nums[2], NextNumber.smallest(nums[2]));
-		assertEquals(1, NextNumber.smallest(nums[3]));
-		assertEquals(nums[4], NextNumber.smallest(nums[4]));
-		assertEquals(7, NextNumber.smallest(nums[5]));
+		pairs.forEach((key, value) -> assertEquals((int) value, NextNumber.getNextSmallest(key)));
 	}
 }
