41d05847fa36e72d427fa3bbc351ce24e5f2f5f1
krystiansulinski <krystian.sulinski@gmail.com>
2020-04-04 07:19:15 +0200

    Add 10.1 (code reduction)

--- SortingAndSearching/SortedMerge/SortedMerge.java
+++ SortingAndSearching/SortedMerge/SortedMerge.java
@@ -9,25 +9,19 @@ package SortedMerge;
 public class SortedMerge {
 	// Time: O(arr.size)
 	// Space: 0(1)
-	public static void sortedMerge(int[] arr, int[] toMerge) {
-		int left = arr.length - toMerge.length - 1; // last index of arr (before the buffer)
-		int right = toMerge.length - 1;
-		int current = arr.length - 1;
-
-		while (left > -1 && right > -1) {
-			if (arr[left] > toMerge[right]) {
-				arr[current] = arr[left];
-				left--;
-			} else {
-				arr[current] = toMerge[right];
-				right--;
-			}
-			current--;
-		}
+	public static void sortedMerge(int[] larger, int[] smaller) {
+		int left = larger.length - smaller.length - 1; // last index of the larger array (before the buffer starts)
+		int right = smaller.length - 1;
+		int current = larger.length - 1;
 
 		while (right > -1) {
-			arr[current] = toMerge[right];
-			right--;
+			if (left > -1 && larger[left] > smaller[right]) {
+				larger[current] = larger[left];
+				left--;
+			} else {
+				larger[current] = smaller[right];
+				right--;
+			}
 			current--;
 		}
 	}
