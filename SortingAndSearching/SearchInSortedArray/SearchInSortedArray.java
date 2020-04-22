a867b09f9ea37bd3631771d515c0fc4b7925f171
krystiansulinski <krystian.sulinski@gmail.com>
2020-04-22 07:38:27 +0200

    Add 10.3 (1h 40min)

--- SortingAndSearching/SearchInSortedArray/SearchInSortedArray.java
+++ SortingAndSearching/SearchInSortedArray/SearchInSortedArray.java
@@ -11,35 +11,75 @@
  */
 package SearchInSortedArray;
 
-import java.util.Arrays;
-
 class SearchInSortedArray {
 	private int count;
 
-	int searchInSortedArray(int[] arr, int key) {
-		this.count = 0;
-		int idx = binarySearch(arr, key, -2, 0, arr.length - 1);
-		System.out.println(Arrays.toString(arr) + ", " + key + " [" + this.count + " / " + arr.length + "]");
-		return idx;
+	int searchBookSolution(int a[], int left, int right, int x) {
+		int mid = (left + right) / 2;
+		if (x == a[mid]) { // Found element
+			return mid;
+		}
+		if (right < left) {
+			return -1;
+		}
+
+		/*
+		 * Either the left or right half must be normally sorted. Find out which side is
+		 * normally ordered, and then use the normally ordered half to figure out which
+		 * side to search to find x.
+		 */
+		if (a[left] < a[mid]) { // Left is normally ordered.
+			if (x >= a[left] && x < a[mid]) {
+				return searchBookSolution(a, left, mid - 1, x); // Search left
+			} else {
+				return searchBookSolution(a, mid + 1, right, x); // Search right
+			}
+		} else if (a[mid] < a[left]) { // Right is normally ordered.
+			if (x > a[mid] && x <= a[right]) {
+				return searchBookSolution(a, mid + 1, right, x); // Search right
+			} else {
+				return searchBookSolution(a, left, mid - 1, x); // Search left
+			}
+		} else if (a[left] == a[mid]) { // Left or right is all repeats
+			if (a[mid] != a[right]) {
+				return searchBookSolution(a, mid + 1, right, x); // Search right
+			} else { // Else, we have to search both halves
+				int result = searchBookSolution(a, left, mid - 1, x); // Search left
+				if (result == -1) {
+					return searchBookSolution(a, mid + 1, right, x); // Search right
+				} else {
+					return result;
+				}
+			}
+		}
+		return -1;
 	}
 
-	private int binarySearch(int[] arr, int key, int prev, int low, int high) {
+	int searchInSortedArray(int[] arr, int key) {
+		this.count = 0;
+		return binarySearch(arr, key, 0, arr.length - 1);
+	}
+
+	// Currently fails for finding 6 in [6, 9, 0, 2, 3, 5]
+	private int binarySearch(int[] arr, int key, int low, int high) {
 		this.count++;
+		if (this.count == arr.length + 2) {
+			return -1; // safe switch
+		}
 		if (low <= high) {
 			int mid = (low + high) / 2;
 			int midVal = arr[mid];
 
 			if (key < midVal) {
 				int nextHigh = mid - 1;
-				if (nextHigh >= 0) {
-					int nextMid = (low + nextHigh) / 2;
-					int nextVal = arr[nextMid];
+				int nextMid = (low + nextHigh) / 2;
+				int nextVal = arr[nextMid];
 
-					if (midVal < nextVal) {
-						low = nextMid + 1;
-					}
+				if (midVal < nextVal) {
+					low = nextMid + 1;
 				}
-				return binarySearch(arr, key, midVal, low, mid - 1);
+
+				return binarySearch(arr, key, low, mid - 1);
 			} else if (key > midVal) {
 				int nextLow = mid + 1;
 				int nextMid = (nextLow + high) / 2;
@@ -48,26 +88,23 @@ class SearchInSortedArray {
 				if (midVal > nextVal) {
 					high = nextMid - 1;
 				}
-				return binarySearch(arr, key, midVal, mid + 1, high);
+				return binarySearch(arr, key, mid + 1, high);
+			} else {
+				return mid;
 			}
-			return mid;
 		} else {
-			int mid = (arr.length - 1) / 2;
+			int mid = (low + arr.length - 1) / 2;
 
 			if (low == arr.length) {
-				return binarySearch(arr, key, prev, 0, mid - 1);
+				return binarySearch(arr, key, 0, mid - 1);
 			} else if (high == -1) {
-				return binarySearch(arr, key, prev, mid + 1, arr.length - 1);
-			} else if (low == mid) {
-				return binarySearch(arr, key, prev, 0, high / 2);
-			} else if (high == mid) {
-				System.out.println(
-						"key: " + key + ", mid:" + mid + ", prev: " + prev + ", high: " + high + ", low: " + low);
-//				return -1;
-				return binarySearch(arr, key, prev, mid + 1, arr.length - 1);
+				return binarySearch(arr, key, mid + 1, arr.length - 1);
+			} else if (mid >= arr.length - 1) {
+				// should not be 8, but incremented low somehow...
+				return binarySearch(arr, key, 0, arr.length - 1);
+			} else {
+				return binarySearch(arr, key, mid + 1, arr.length - 1);
 			}
 		}
-
-		return -1;
 	}
 }
--- SortingAndSearching/SearchInSortedArray/SearchInSortedArrayTest.java
+++ SortingAndSearching/SearchInSortedArray/SearchInSortedArrayTest.java
@@ -2,51 +2,50 @@ package SearchInSortedArray;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+
+import Helper.ArraysHelper;
 
 public class SearchInSortedArrayTest {
 	@Test
-	public void searchInSortedArray() {
-		int[][] arr = { { 0, 2, 3, 5, 6, 9 }, { 2, 3, 5, 6, 9, 0 }, { 2, 5, 6, 9, 0, 3 }, { 5, 6, 9, 0, 2, 3 },
+	public void searchBookSolution1() {
+		int[][] arr = { { 0, 2, 3, 5, 6, 9 }, { 2, 3, 5, 6, 9, 0 }, { 3, 5, 6, 9, 0, 2 }, { 5, 6, 9, 0, 2, 3 },
 				{ 6, 9, 0, 2, 3, 5 }, { 9, 0, 2, 3, 5, 6 } };
 
 		SearchInSortedArray search = new SearchInSortedArray();
 		for (int[] element : arr) {
 			for (int j = 0; j < element.length; j++) {
-				assertEquals(j, search.searchInSortedArray(element, element[j]));
+				assertEquals(j, search.searchBookSolution(element, 0, element.length - 1, element[j]));
 			}
 		}
 	}
 
-//	@Test
-//	public void searchInSortedArray() {
-//		int[] arr = { 15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14 };
-//
-//		SearchInSortedArray search = new SearchInSortedArray();
-//		for (int i = 0; i < arr.length; i++) {
-//			assertEquals(i, search.searchInSortedArray(arr, arr[i]));
-//		}
-//	}
-//
-//	@Test
-//	public void searchInSortedArray2() {
-//		int[] arr = { 9, 10, 11, 1, 2, 3, 4, 5, 6, 7, 8 };
-//
-//		SearchInSortedArray search = new SearchInSortedArray();
-//		for (int i = 0; i < arr.length; i++) {
-//			assertEquals(i, search.searchInSortedArray(arr, arr[i]));
-//		}
-//	}
+	@Test
+	public void searchBookSolution2() {
+		final int LENGTH = 10;
+		int[] arr = ArraysHelper.getRandomSortedArray(LENGTH);
+		arr = ArraysHelper.rotateRandomly(arr);
 
-//	@Test
-//	public void searchInSortedArray3() {
-//		final int LENGTH = 10;
-//		int[] arr = ArraysHelper.getRandomSortedArray(LENGTH);
-//		arr = ArraysHelper.rotateRandomly(arr);
-//
-//		SearchInSortedArray search = new SearchInSortedArray();
-//		for (int element : arr) {
-//			assertEquals(element, arr[search.searchInSortedArray(arr, element)]);
-//		}
-//	}
+		SearchInSortedArray search = new SearchInSortedArray();
+		for (int element : arr) {
+			assertEquals(element, arr[search.searchBookSolution(arr, 0, arr.length - 1, element)]);
+		}
+	}
+
+	// Fails from time to time
+	@Test
+	public void searchInSortedArray() {
+		final int LENGTH = 10;
+		int[] arr = ArraysHelper.getRandomSortedArray(LENGTH);
+		arr = ArraysHelper.rotateRandomly(arr);
+
+		SearchInSortedArray search = new SearchInSortedArray();
+		for (int element : arr) {
+			int result = search.searchInSortedArray(arr, element);
+			System.out.println("Find " + element + " in " + Arrays.toString(arr) + ", Result: " + result);
+			assertEquals(element, arr[result]);
+		}
+	}
 }
