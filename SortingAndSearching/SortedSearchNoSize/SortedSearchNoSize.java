816c740102df5616d8aa2786302e5c4c3fdddc7b
krystiansulinski <krystian.sulinski@gmail.com>
2020-04-28 07:55:43 +0200

    Add 10.4 (1h 40min)

--- SortingAndSearching/SortedSearchNoSize/SortedSearchNoSize.java
+++ SortingAndSearching/SortedSearchNoSize/SortedSearchNoSize.java
@@ -2,53 +2,69 @@ package SortedSearchNoSize;
 
 import Helper.Listy;
 
+/* 10.4 Sorted Search, No Size
+ * You are given an array-like data structure Listy which lacks a size method.
+ * It does, however, have an elementAt(i) method that returns the element at index i in O(1) time.
+ * If i is beyond the bounds of the data structure, it returns -1. (For this reason, the data
+ * structure only supports positive integers.) Given a Listy which contains sorted, positive integers,
+ * find the index at which an element x occurs. If x occurs multiple times, you may return any index.
+ */
 public class SortedSearchNoSize {
-	public static int getSize(Listy list, boolean log) {
+	public static int sortedSearchNoSize(Listy list, int x) {
 		if (list == null) {
 			return -1;
 		}
 
 		int low = 0;
 		int high = 1;
-		int mid = -1;
 
-		while (high != mid) {
-			int value = list.elementAt(mid == -1 ? high : mid);
+		/* Find out of range index */
+		while (list.elementAt(high) != -1) {
+			low = high;
+			high = high * 2;
+		}
 
-			if (value != -1) {
-				if (mid == -1) {
-					if (log) {
-						System.out.print(" a ");
-					}
-					low = high;
-					high = high * 2;
-				} else {
-					if (log) {
-						System.out.print(" b ");
-					}
-					low = mid + 1;
-					mid = (low + high) / 2;
-				}
+		/* Find last index */
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			int midValue = list.elementAt(mid);
+
+			if (midValue == -1) {
+				high = mid - 1;
 			} else {
-				if (mid == -1) {
-					if (log) {
-						System.out.print(" c ");
-					}
-					high = high - 1;
-				} else {
-					if (log) {
-						System.out.print(" d ");
-					}
-					high = mid - 1;
-				}
-				mid = (low + high) / 2;
-			}
-
-			if (log) {
-				System.out.print("\n" + low + ", " + high + ", " + mid + ", " + value);
+				low = mid + 1;
 			}
 		}
 
-		return high + 1;
+		return binarySearch(list, x, 0, low - 1);
+	}
+
+	private static int binarySearch(Listy list, int x, int low, int high) {
+		boolean inAscendingOrder = list.elementAt(low) <= list.elementAt(high);
+
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			int midValue = list.elementAt(mid);
+
+			if (inAscendingOrder) {
+				if (x < midValue) {
+					high = mid - 1;
+				} else if (x > midValue) {
+					low = mid + 1;
+				} else {
+					return mid;
+				}
+			} else {
+				if (x < midValue) {
+					low = mid + 1;
+				} else if (x > midValue) {
+					high = mid - 1;
+				} else {
+					return mid;
+				}
+			}
+		}
+
+		return -1;
 	}
 }
--- SortingAndSearching/SortedSearchNoSize/SortedSearchNoSizeTest.java
+++ SortingAndSearching/SortedSearchNoSize/SortedSearchNoSizeTest.java
@@ -1,6 +1,9 @@
 package SortedSearchNoSize;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.junit.Test;
 
@@ -9,23 +12,34 @@ import Helper.Listy;
 
 public class SortedSearchNoSizeTest {
 	@Test
-	public void indexOfTest() {
-		final int MAX_LENGTH = 17;
-		ArrayList<Listy> listOflisty = new ArrayList<>();
+	public void sortedSearchNoSizeTest() {
+		final int MAX_LENGTH = 20;
 
-		for (int i = 0; i < 100; i++) {
-			int randomSize = (int) (Math.random() * MAX_LENGTH);
-			Listy listy = new Listy(ArraysHelper.getRandomSortedList(randomSize));
-			listOflisty.add(listy);
+		for (int i = 0; i < 20; i++) {
+			ArrayList<Integer> list = getList(1 + (int) (Math.random() * MAX_LENGTH));
 
-			int size = SortedSearchNoSize.getSize(listy, false);
-			if (size != randomSize) {
-				System.out.println(listy + ", " + randomSize + " != " + size);
-				SortedSearchNoSize.getSize(listy, true);
-				return;
+			if (i % 2 == 0) {
+				list.sort(Collections.reverseOrder());
+			}
+
+			int value = list.get((int) (Math.random() * (list.size())));
+			int index = SortedSearchNoSize.sortedSearchNoSize(new Listy(list), value);
+			int firstIndex = list.indexOf(value);
+			int lastIndex = list.lastIndexOf(value);
+
+			System.out.println(list + ".indexOf(" + value + ") = " + index);
+			assertTrue(firstIndex <= index && index <= lastIndex);
+		}
+	}
+
+	private ArrayList<Integer> getList(int randomSize) {
+		ArrayList<Integer> list = new ArrayList<>();
+
+		for (int val : ArraysHelper.getRandomSortedList(randomSize)) {
+			if (val > 0) {
+				list.add(val);
 			}
 		}
-
-//			assertTrue(list.indexOf(element) <= index && index <= list.lastIndexOf(element));
+		return list;
 	}
 }
