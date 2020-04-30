cb64674a61354602dabeb7422947cc7432b7c8da
krystiansulinski <krystian.sulinski@gmail.com>
2020-04-30 07:50:09 +0200

    Add 10.6 (15 min)

--- SortingAndSearching/SparseSearch/SparseSearchTest.java
+++ SortingAndSearching/SparseSearch/SparseSearchTest.java
@@ -5,17 +5,17 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SparseSearchTest {
-//	@Test
-//	public void sparseSearchTest() {
-//		String[] arr = { "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", "" };
-//
-//		for (String s : arr) {
-//			if (s.length() != 0) {
-//				assertEquals(linearSearch(arr, s), SparseSearch.sparseSearch(arr, s));
-//			}
-//		}
-//
-//	}
+	@Test
+	public void sparseSearchTest() {
+		String[] arr = { "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", "" };
+
+		for (String s : arr) {
+			if (s.length() != 0) {
+				assertEquals(linearSearch(arr, s), SparseSearch.sparseSearch(arr, s));
+			}
+		}
+
+	}
 
 	@Test
 	public void sparseSearchTest2() {
