20d9386aa2129957e65a21755421faa0819e63d7
Krystian Sulinski <krystian.sulinski@gmail.com>
2020-10-06 08:41:51 +0200

    Add 1.7 Rotate Matrix WIP (50 min)

--- Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/RotateMatrix.java
+++ Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/RotateMatrix.java
@@ -5,88 +5,18 @@
 package Q1_07_Rotate_Matrix;
 
 public class RotateMatrix {
-    public static void rotateMatrix(final int[][] arr, final int N) {
+    public static int[][] rotateRightBy90Degrees(final int[][] arr) {
         // assume arr is NxN
         // assume values in arr are different
-
+        final int N = arr.length;
         int[][] rotated = new int[N][N];
-        boolean nIsOdd = N % 2 == 1;
-
-        for (int col = 0; col < N; col++) {
-            if (nIsOdd && N / 2 - 1 == col) {
-                rotated = swapMid(arr, rotated, N, col);
-            } else {
-                rotated = swap(arr, rotated, N, col);
-            }
-        }
-
-    }
-
-    private static int[][] swapMid(final int[][] arr, final int[][] rotated, final int N, final int col) {
-        int[][] swapped = rotated.clone();
 
         for (int row = 0; row < N; row++) {
-            swapped[row][col] = arr[col][row];
-        }
-
-        return swapped;
-    }
-
-    private static int[][] swap(final int[][] arr, final int[][] rotated, final int N, final int col) {
-        int[][] swapped = rotated.clone();
-
-        for (int row = 0; row < N; row++) {
-            // to be continued
-            swapped[row][col] = rotated
-        }
-
-        return swapped;
-    }
-
-    public void rotate(int[][] arr, int rowIdxFrom, int colIdxFrom) {
-        int N = arr.length;
-        int maxIndex = N - 1;
-
-        int temp;
-        for (int i = 0; i < 4; i++) {
-            int value = arr[i];
-            int rowIdxTo = rowIdxFrom;
-            int colIdxTo = colIdxFrom;
-            boolean isLastRow = rowIdxTo == maxIndex;
-            boolean isLastCol = colIdxTo == maxIndex;
-
-            for (int j = 0; j < maxIndex; j++) {
-                if (colIdxTo < maxIndex) {
-                    colIdxTo++;
-                } else if (rowIdxTo < maxIndex) {
-                    rowIdxTo++;
-                } else if (isLastRow && isLastCol) {
-                    colIdxTo--;
-                } else if (isLastRow && !isLastCol) {
-                    rowIdxTo--;
-                } else if (!isLastRow && isLastCol) {
-                    rowIdxTo++;
-                }
-            }
-
-            temp = arr[rowIdxTo][colIdxTo];
-            arr[rowIdxTo][colIdxTo] = arr[rowIdxFrom][colIdxFrom];
-        }
-    }
-
-    public int[] getNextIndeces(int rowIdx, int colIdx, int length) {
-        int[][] indeces = new int[1][1];
-        int lastIndex = length - 1;
-
-        boolean reachLastRow = rowIdx == lastIndex;
-        boolean reachedLastCol = colIdx == lastIndex;
-
-        for (int i = 0; i < lastIndex; i++) {
-            for (int j = 0; j < lastIndex; j++) {
-
+            for (int col = 0; col < N; col++) {
+                rotated[row][col] = arr[N - 1 - col][row];
             }
         }
 
-        return indeces;
+        return rotated;
     }
 }
--- Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/RotateMatrixTest.java
+++ Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/RotateMatrixTest.java
@@ -1,4 +1,58 @@
 package Q1_07_Rotate_Matrix;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
 public class RotateMatrixTest {
+    @Test
+    public void rotate2DMatrix() {
+        final int[][] arr = {
+                {1, 2},
+                {3, 4}
+        };
+
+        final int[][] rotated = {
+                {3, 1},
+                {4, 2}
+        };
+
+        assertArrayEquals(rotated, RotateMatrix.rotateRightBy90Degrees(arr));
+    }
+
+    @Test
+    public void rotate3DMatrix() {
+        final int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        final int[][] rotated = {
+                {7, 4, 1},
+                {8, 5, 2},
+                {9, 6, 3}
+        };
+
+        assertArrayEquals(rotated, RotateMatrix.rotateRightBy90Degrees(arr));
+    }
+
+    @Test
+    public void rotate4DMatrix() {
+        final int[][] arr = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+
+        final int[][] rotated = {
+                {13, 9, 5, 1},
+                {14, 10, 6, 2},
+                {15, 11, 7, 3},
+                {16, 12, 8, 4}
+        };
+
+        assertArrayEquals(rotated, RotateMatrix.rotateRightBy90Degrees(arr));
+    }
 }
