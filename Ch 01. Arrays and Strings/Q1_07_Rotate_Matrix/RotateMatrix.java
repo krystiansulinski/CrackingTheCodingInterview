d474b123635811b63922293b7de44b8716626883
Krystian Sulinski <krystian.sulinski@gmail.com>
2020-11-23 08:15:38 +0100

    Add 1.7 Rotate Matrix WIP (35 min)

--- Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/RotateMatrix.java
+++ Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/RotateMatrix.java
@@ -28,24 +28,28 @@ public class RotateMatrix {
 
 //        for (int col = 0; col < N / 2; col++) {
 //            for (int row = 0; row < N / 2 + 1; row++) {
-        for (int row = 0; row < N / 2; row++) {
-            for (int col = 0; col < N / 2 + 1; col++) {
-                System.out.println(row + " " + col);
+//        final int maxIdx = N - 1;
+        for (int row = 0; row < Math.floor(N / 2); row++) {
+            for (int col = 0; col < N; col++) {
 //                int temp = arr[col][maxIdx - row];
 //                arr[col][maxIdx - row] = arr[row][col];
 //                arr[row][col] = temp;
-
+//
 //                temp = arr[maxIdx - row][maxIdx - col];
 //                arr[maxIdx - row][maxIdx - col] = arr[row][col];
 //                arr[row][col] = temp;
-
+//
 //                temp = arr[maxIdx - col][row];
 //                arr[maxIdx - col][row] = arr[row][col];
 //                arr[row][col] = temp;
 
+//                swap(arr, row, col, col, M - row);
+//                swap(arr, row, col, M - row, M - col);
+//                swap(arr, row, col, M - col, row);
+
                 swap(arr, row, col, col, M - row);
-                swap(arr, row, col, M - row, M - col);
-                swap(arr, row, col, M - col, row);
+//                swap(arr, row, col, M - row, M - col);
+//                swap(arr, row, col, M - col, row);
             }
         }
     }
--- Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/RotateMatrixTest.java
+++ Ch 01. Arrays and Strings/Q1_07_Rotate_Matrix/RotateMatrixTest.java
@@ -60,9 +60,10 @@ public class RotateMatrixTest {
 
     @Test
     public void rotate2DMatrixInPlace() {
+        System.out.println(printMatrix(arr2D));
+        System.out.println(printMatrix(rotated2D));
         RotateMatrix.rotateRightInPlace(arr2D);
-        System.out.println(Arrays.deepToString(rotated2D));
-        System.out.println(Arrays.deepToString(arr2D));
+        System.out.println(printMatrix(arr2D));
 
         assertArrayEquals(rotated2D, arr2D);
     }
@@ -78,10 +79,25 @@ public class RotateMatrixTest {
 
     @Test
     public void rotate4DMatrixInPlace() {
+        System.out.println(printMatrix(arr4D));
+        System.out.println(printMatrix(rotated4D));
         RotateMatrix.rotateRightInPlace(arr4D);
-        System.out.println(Arrays.deepToString(rotated4D));
-        System.out.println(Arrays.deepToString(arr4D));
+        System.out.println(printMatrix(arr4D));
+
 
         assertArrayEquals(rotated4D, arr4D);
+    }
+
+    public String printMatrix(int[][] mat) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[row].length; col++) {
+                sb.append(mat[row][col]).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        return sb.toString();
     }
 }
