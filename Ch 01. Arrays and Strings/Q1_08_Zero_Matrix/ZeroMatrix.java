95b70f97b43c93c065999cc0fdfb3d3b133f60a4
Krystian Sulinski <krystian.sulinski@gmail.com>
2020-11-27 08:16:57 +0100

    Add 1.8 Zero Matrix (5 min)

--- Ch 01. Arrays and Strings/Q1_08_Zero_Matrix/ZeroMatrix.java
+++ Ch 01. Arrays and Strings/Q1_08_Zero_Matrix/ZeroMatrix.java
@@ -14,6 +14,7 @@ public class ZeroMatrix {
         final HashSet<Integer> rows = new HashSet<>();
         final HashSet<Integer> cols = new HashSet<>();
 
+        // Test
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 if (matrix[row][col] == 0) {
